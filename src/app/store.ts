bc7dc0ba4a083b18e7aa51c4b57c465517973da3
CodyAdam <cody.ad4@gmail.com>
2021-07-05 21:14:16 +0200

    Add redux-undo and the board reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,20 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import boardReducer from '../features/board/boardSlice';
+import undoable from 'redux-undo';
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+    reducer: {
+        counter: counterReducer,
+        board: undoable(boardReducer),
+    },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
 >;
