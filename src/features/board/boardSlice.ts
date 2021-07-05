1de5334d131e7ee121aca5db7481c844da19b2ae
CodyAdam <cody.ad4@gmail.com>
2021-07-05 21:13:21 +0200

    Created Redux logic for the Board

---
+++ src/features/board/boardSlice.ts
@@ -0,0 +1,60 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../app/store';
+import { BoardState, Tile } from './boardSlice.d.ts'
+
+
+function updateScore(state: BoardState): { p1: Array<string>, p2: Array<string> } {
+  let p1: Array<string> = [];
+  let p2: Array<string> = [];
+  return { p1, p2 };
+}
+
+function getWinner(state: BoardState): false | 1 | 2 {
+  if (state.score.p1.length >= state.scoreToWin)
+    return 1;
+  else if (state.score.p2.length >= state.scoreToWin)
+    return 2
+  else return false;
+}
+
+const initialState: BoardState = {
+  scoreToWin: 1,
+  score: { p1: [], p2: [] },
+  turn: 1,
+  winner: false,
+  grid: [
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+  ]
+}
+
+export const boardSlice = createSlice({
+  name: 'board',
+  initialState,
+  reducers: {
+    playAt: (state, action: PayloadAction<number>) => {
+      const cell = action.payload;
+      const player = state.turn;
+      if (state.grid[cell].slotsIndex < 3) {
+        state.grid[cell].slotsIndex++;
+        state.grid[cell].value.push(player);
+        state.turn = player === 1 ? 2 : 1;
+        state.winner = getWinner(state);
+      }
+    },
+  }
+});
+
+export const { playAt } = boardSlice.actions;
+
+
+export const selectGrid = (state: RootState) => state.board.present.grid;
+
+export default boardSlice.reducer;
