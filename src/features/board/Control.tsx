ed016941f69d6403d7c536e9bbaa159e1b1699a6
CodyAdam <cody.ad4@gmail.com>
2021-07-06 16:13:52 +0200

    Add buttons functionnality

--- src/features/board/Control.tsx
+++ src/features/board/Control.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import styles from './Board.module.css';
+import { ActionCreators } from 'redux-undo';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { reset } from './boardSlice';
 
 export function Control() {
+    const dispatch = useAppDispatch();
+    const canUndo: boolean = useAppSelector((state) => state.board.past.length > 0);
+    const canRedo: boolean = useAppSelector((state) => state.board.future.length > 0);
     return (
         <div className={styles.controlBar}>
-            <button className={styles.controlButton}>undo</button>
-            <button className={styles.controlButton}>redo</button>
-            <button className={styles.controlButton}>reset</button>
+            <button
+                className={styles.controlButton}
+                onClick={() => dispatch(ActionCreators.undo())}
+                disabled={!canUndo}
+            >
+                <div className={styles.controlButtonInside}>undo</div>
+            </button>
+            <div className={styles.controlButtonContainer}>
+                <button
+                    className={styles.controlButton}
+                    onClick={() => dispatch(ActionCreators.redo())}
+                    disabled={!canRedo}
+                >
+                    <div className={styles.controlButtonInside}>redo</div>
+                </button>
+            </div>
+            <div className={styles.controlButtonContainer}>
+                <button className={styles.controlButton} onClick={() => dispatch(reset())}>
+                    <div className={styles.controlButtonInside}>reset</div>
+                </button>
+            </div>
         </div>
     );
 }
